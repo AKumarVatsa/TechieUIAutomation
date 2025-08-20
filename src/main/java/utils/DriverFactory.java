c35b3332ae3739a76a460f5673925cdc4d2d6920
rekharam392 <rekharam392@gmail.com>
2025-08-20 23:15:01 +0530

    pipeline test case failure fix

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -3,26 +3,37 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
     private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
 
     // Initialize driver and return it
+	/*
+	 * public static WebDriver initDriver() { if (tlDriver.get() == null) {
+	 * System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	 * 
+	 * ChromeOptions options = new ChromeOptions();
+	 * options.addArguments("--start-maximized"); // open browser maximized
+	 * 
+	 * WebDriver driver = new ChromeDriver(options); tlDriver.set(driver); } return
+	 * tlDriver.get(); }
+	 */
+    
+
     public static WebDriver initDriver() {
         if (tlDriver.get() == null) {
-            System.setProperty("webdriver.chrome.driver",
-                    "chromedriver.exe");
-
+            WebDriverManager.chromedriver().setup();  // ✅ auto-detects OS & Chrome version
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--start-maximized");  // open browser maximized
-
+            options.addArguments("--start-maximized");
+            //options.addArguments("--no-sandbox");
+           // options.addArguments("--disable-dev-shm-usage");
             WebDriver driver = new ChromeDriver(options);
             tlDriver.set(driver);
         }
         return tlDriver.get();
     }
-
     // Get current driver
     public static WebDriver getDriver() {
         return tlDriver.get();
